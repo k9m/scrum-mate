2991bc2308067c6e0dbd95ceada329969296aa44
Maksa, Greg (ELS-AMS) <g.maksa@elsevier.com>
2019-02-12 15:54:10 +0100

    Styling changes

--- scrum-mate-python/httpServer.py
+++ scrum-mate-python/httpServer.py
@@ -1,3 +1,5 @@
+import os
+
 import cherrypy
 
 from service.ocr_service import process
@@ -21,25 +23,41 @@ class Root(object):
         return """
 <html>
 <body>
-    <div>
-        <form method="POST" enctype="multipart/form-data" action="/upload">
-            <table>
-                <tr>
-                    <td>File to upload:</td>
-                    <td><input type="file" name="file"/></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><input type="submit" value="Upload"/></td>
-                </tr>
-            </table>
-        </form>
+    <div class="container" align="center">
+            <div class="col-md-6" align="center">
+                <h2 class="display-4">Scrum8 uploader</h2>
+                
+                <form method="POST" enctype="multipart/form-data" action="/upload">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <input type="submit" class="btn btn-primary" value="Upload"/>
+                  </div>
+                  <div class="custom-file">
+                    <input type="file" class="custom-file-input"  name="file" >
+                    <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                  </div>
+                </div>
+                 </form>
+            </div>
     </div>
+    <link rel="stylesheet" href="static/bootstrap.min.css"/>
+    <script src="static/bootstrap.bundle.min.js"></script>
 </body>
 </html>
 """
 
 
+conf = {
+    '/': {
+        'tools.sessions.on': True,
+        'tools.staticdir.root': os.path.abspath(os.getcwd())
+    },
+    '/static': {
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': 'assets'
+    }
+}
+
 cherrypy.config.update({'server.socket_host': '0.0.0.0'})
 cherrypy.config.update({'server.socket_port': 8081})
-cherrypy.quickstart(Root())
+cherrypy.quickstart(Root(), '/', conf)
