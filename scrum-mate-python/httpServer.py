cb895d3c91bfc7310c9bcb52df4b6338d95fc844
Maksa, Greg (ELS-AMS) <g.maksa@elsevier.com>
2019-02-12 11:10:48 +0100

    Python server

---
+++ scrum-mate-python/httpServer.py
@@ -0,0 +1,42 @@
+import cherrypy
+import simplejson
+
+class Root(object):
+
+    @cherrypy.expose
+    def upload(self, file):
+        cl = cherrypy.request.headers['Content-Length']
+        data = file.file.read(8192)
+        print(data)
+        # TODO do the stuff
+
+        # body = simplejson.loads(rawbody)
+        # do_something_with(body)
+        return "Blah"
+
+    @cherrypy.expose
+    def index(self):
+        return """
+<html>
+<body>
+
+<div>
+    <form method="POST" enctype="multipart/form-data" action="/upload">
+        <table>
+            <tr>
+                <td>File to upload:</td>
+                <td><input type="file" name="file"/></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="Upload"/></td>
+            </tr>
+        </table>
+    </form>
+</div>
+
+</body>
+</html>
+"""
+
+cherrypy.quickstart(Root())
