787c3fdabf0dee1ab002f6c52b4ad219c182308c
Canberk Oguz <c.oguz@elsevier.com>
2019-02-12 14:52:24 +0100

    added ocr_service

---
+++ scrum-mate-python/service/ocr_service.py
@@ -0,0 +1,60 @@
+import re
+
+import cv2
+import numpy as np
+import pytesseract
+from pyzbar.pyzbar import decode, ZBarSymbol
+
+JIRA_TICKET_PREFIX = 'SCRM8'
+
+
+def crop_image(image, start_column, end_column):
+    height, width = image.shape[:2]
+    start_row, end_row = int(0), int(height)
+    cropped_image = image[start_row:end_row, start_column:end_column]
+    return cropped_image
+
+
+def split_image_to_columns(image):
+    columns = []
+    height, width = image.shape[:2]
+
+    decoded = decode(image, symbols=[ZBarSymbol.QRCODE])
+
+    qr_dic = {}
+    for qr in decoded:
+        x = qr[2][0]  # The Left position of the QR code
+        qr_dic[x] = qr[0]  # The Data stored in the QR code
+
+    sorted_qr_list = sorted(qr_dic.keys())
+
+    for i in range(len(sorted_qr_list)):
+        x_axis = sorted_qr_list[i]
+        start_column = x_axis
+
+        if i < len(sorted_qr_list) - 1:
+            end_column = sorted_qr_list[i + 1]
+        else:
+            end_column = width
+
+        cropped_image = crop_image(image, start_column, end_column)
+        columns.append(cropped_image)
+
+    return columns
+
+
+def get_ticket_numbers(text, prefix):
+    return re.findall(f"{prefix}-[0-9]\\d*", text)
+
+
+def process(image_file):
+    img = cv2.imdecode(np.frombuffer(image_file, np.uint8), -1)
+    columns = split_image_to_columns(img)
+
+    for i in range(len(columns)):
+        text = pytesseract.image_to_string(columns[i])
+        tickets = get_ticket_numbers(text, JIRA_TICKET_PREFIX)
+        print('column' + str(i))
+        print(tickets)
+
+    return columns
