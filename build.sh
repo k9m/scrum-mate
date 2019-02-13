4a4911af8f9039265274a8370e82d14fdd1552c2
Maksa, Greg (ELS-AMS) <g.maksa@elsevier.com>
2019-02-13 16:39:47 +0100

    THE ONE AND ONLY TAR FILES 2.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cat artifacts/jira.tar.gz.part* > build/jira.tar.gz
-gradle clean untar :scrum8-service:bootJar
+gradle untar :scrum8-service:bootJar
 docker-compose build
 docker-compose up -d
 
