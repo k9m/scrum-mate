bf896c6943366c085451d5f11b3a76e23529853a
Canberk Oguz <c.oguz@elsevier.com>
2019-02-13 17:16:53 +0100

    added jira update to image upload

---
+++ scrum8-ocr/service/jira_service.py
@@ -0,0 +1,16 @@
+import requests
+
+JIRA_UPDATE_URL = 'http://localhost:8082/tickets/'
+
+
+def update_tickets(tickets_dic):
+    response_dic = {}
+    for status, tickets in tickets_dic.items():
+        for ticket in tickets:
+            payload = {'targetState': status}
+            response = requests.post(JIRA_UPDATE_URL + ticket, json=payload)
+            if response.ok:
+                response_dic[ticket] = 'Updated!'
+            else:
+                response_dic[ticket] = response.json()['message']
+    return response_dic
